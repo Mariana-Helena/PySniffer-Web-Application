3d0d18d6e9021edb10d7e4f6f8c1e3dd1db860e3
Mariana Helena <marianahines@hotmail.com>
2021-11-18 20:55:52 -0300

    replacing images

--- src/components/gallery/photos.tsx
+++ src/components/gallery/photos.tsx
@@ -1,46 +1,41 @@
 export const photos = [
     {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img1.PNG",
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img2.PNG",
       width: 1,
       height: 1
     },
     {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img3.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img4.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img5.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img6.PNG",
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img7.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img8.PNG",
       width: 4,
       height: 3
     }
