ffd0c31cf2cca90a8c149bde99a1a6b415a5b579
Mariana Helena <marianahines@hotmail.com>
2021-11-18 20:56:57 -0300

    fixing image url

--- src/components/gallery/photos.tsx
+++ src/components/gallery/photos.tsx
@@ -1,41 +1,41 @@
 export const photos = [
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img1.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image1.PNG",
       width: 4,
       height: 3
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img2.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image2.PNG",
       width: 1,
       height: 1
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img3.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image3.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img4.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image4.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img5.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image5.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img6.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image6.PNG",
       width: 4,
       height: 3
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img7.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image7.PNG",
       width: 3,
       height: 4
     },
     {
-      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/img8.PNG",
+      src: "https://github.com/Mariana-Helena/PySniffer-Web-Application/blob/main/src/data/statistics/image8.PNG",
       width: 4,
       height: 3
     }
